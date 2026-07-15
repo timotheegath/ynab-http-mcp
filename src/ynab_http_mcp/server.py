3a5e9295db16ce73f3fd6a81d3494e4b3971065d
timotheegath <timothee.gathmann@ik.me>
2026-07-15 14:36:56 +0100

    Setting the PLAN_ID in server.py

--- src/ynab_http_mcp/server.py
+++ src/ynab_http_mcp/server.py
@@ -1,5 +1,5 @@
 from ynab_http_mcp.debug import is_debug_enabled, debug_ynab_response, debug_exception
-
+from ynab_http_mcp.plans import set_default_plan
 from typing import Any
 from uuid import UUID
 
@@ -18,25 +18,16 @@ mcp = FastMCP("weather")
 # Logging
 
 
-# Credentials
+# Credentials / Config
 YNAB_CONFIG = ynab.Configuration(access_token=os.getenv("YNAB_API_KEY"))
 
 # Set plan
-
+PLAN_ID = set_default_plan(YNAB_CONFIG)
+            
+ 
+            
 with ynab.ApiClient(YNAB_CONFIG) as api_client:
     plans_api = ynab.PlansApi(api_client)
     plans_response = plans_api.get_plans()
     debug_ynab_response("Plans: ", plans_response)
-
-def set_default_plan() -> UUID:
-    plan_id = os.getenv("YNAB_PLAN_ID")
-    if plan_id is None:
-        with ynab.ApiClient(YNAB_CONFIG) as api_client:
-            plans_api = ynab.PlansApi(api_client)
-            plans_response = plans_api.get_plans()
-            
-    return UUID(plan_id)
-
-PLAN_ID 
-            
 
